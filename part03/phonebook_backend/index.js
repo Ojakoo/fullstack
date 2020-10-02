48055048d208e93039b28d400a8948e391079983
ojakoo <ilari.ojakorpi@gmail.com>
2020-10-02 15:38:58 +0300

    part03

---
+++ part03/phonebook_backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json()) 
+
+let notes = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+         number: "040-123456"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
+    },
+    {
+        id: 2,
+        name: "Mary Poppendick",
+        number: "39-23-6423122"
+    }   
+]
