d7014225be8a1e4513e58f0518a0e401889ee62a
ojakoo <ilari.ojakorpi@gmail.com>
2020-10-05 16:02:45 +0300

    build

--- part03/phonebook_backend/index.js
+++ part03/phonebook_backend/index.js
@@ -8,6 +8,7 @@ const app = express()
 app.use(cors())
 
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token('body', (req, res) => JSON.stringify(req.body)); 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
