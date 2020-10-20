945f5c10a13f500f89b851e09ad95829825304df
ojakoo <ilari.ojakorpi@gmail.com>
2020-10-20 17:33:21 +0300

    part04

---
+++ part04/bloglistbackend/utils/config.js
@@ -0,0 +1,11 @@
+const { Model } = require('mongoose')
+
+require('dotenv').config()
+
+let PORT = process.env.PORT
+let MONGODB_URI = process.env.MONGODB_URI
+
+module.exports = {
+    MONGODB_URI,
+    PORT
+}
