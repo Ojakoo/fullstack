968cb8c5485641d06adf4392a36c64f56c2616a6
ojakoo <ilari.ojakorpi@gmail.com>
2020-10-23 16:20:15 +0300

    20

--- part04/bloglistbackend/utils/middleware.js
+++ part04/bloglistbackend/utils/middleware.js
@@ -29,7 +29,13 @@ const errorHandler = (error, request, response, next) => {
 }
 
 const tokenExtractor = (request, response, next) => {
-  
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  } else {
+    request.token = null
+  }
 
   next()
 }
