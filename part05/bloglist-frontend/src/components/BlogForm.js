c884630ae411a9bcc6890ca7cb85ff98d0408aea
ojakoo <ilari.ojakorpi@gmail.com>
2020-10-28 12:47:23 +0200

    lint

--- part05/bloglist-frontend/src/components/BlogForm.js
+++ part05/bloglist-frontend/src/components/BlogForm.js
@@ -5,49 +5,49 @@ import UrlForm from './InputForms/UrlForm'
 import PropTypes from 'prop-types'
 
 const BlogForm = ({ createBlog }) => {
-    const [title, setTitle] = useState('')
-    const [author, setAuthor] = useState('')
-    const [url, setUrl] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
-    const handleTitleChange = (event) => {
-        setTitle(event.target.value)
-    }
-    
-    const handleAuthorChange = (event) => {
-        setAuthor(event.target.value)
-    }
-    
-    const handleUrlChange = (event) => {
-        setUrl(event.target.value)
-    }
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value)
+  }
 
-    const addBlog = (event) => {
-        event.preventDefault()
-        createBlog({
-            title: title,
-            author: author,
-            url: url,
-        })
+  const handleAuthorChange = (event) => {
+    setAuthor(event.target.value)
+  }
 
-        setAuthor('')
-        setTitle('')
-        setUrl('')
-    }
+  const handleUrlChange = (event) => {
+    setUrl(event.target.value)
+  }
 
-    return (
-        <form onSubmit={addBlog}>
-            <TitleForm title={title} handleTitleChange={handleTitleChange} />
-            <AuthorForm author={author} handleAuthorChange={handleAuthorChange} />
-            <UrlForm url={url} handleUrlChange={handleUrlChange} />
-            <div>
-            <button type="submit">create</button>
-            </div>
-        </form>
-    )
+  const addBlog = (event) => {
+    event.preventDefault()
+    createBlog({
+      title: title,
+      author: author,
+      url: url,
+    })
+
+    setAuthor('')
+    setTitle('')
+    setUrl('')
+  }
+
+  return (
+    <form onSubmit={addBlog}>
+      <TitleForm title={title} handleTitleChange={handleTitleChange} />
+      <AuthorForm author={author} handleAuthorChange={handleAuthorChange} />
+      <UrlForm url={url} handleUrlChange={handleUrlChange} />
+      <div>
+        <button type="submit">create</button>
+      </div>
+    </form>
+  )
 }
 
 BlogForm.propTypes = {
-    createBlog: PropTypes.func.isRequired
+  createBlog: PropTypes.func.isRequired
 }
 
 export default BlogForm
--- part05/bloglist-frontend/src/components/LoginForm.js
+++ part05/bloglist-frontend/src/components/LoginForm.js
@@ -4,23 +4,23 @@ import UsernameForm from './InputForms/UsernameForm'
 import PasswordForm from './InputForms/PasswordForm'
 
 const LoginForm = ({ handleLogin, username, handleUsernameChange, password, handlePasswordChange }) => {
-    return (
-        <form onSubmit={handleLogin}>
-            <UsernameForm username={username} handleUsernameChange={handleUsernameChange} />
-            <PasswordForm password={password} handlePasswordChange={handlePasswordChange} />
-            <div>
-            <button type="submit">login</button>
-            </div>
-        </form>
-    )
+  return (
+    <form onSubmit={handleLogin}>
+      <UsernameForm username={username} handleUsernameChange={handleUsernameChange} />
+      <PasswordForm password={password} handlePasswordChange={handlePasswordChange} />
+      <div>
+        <button type="submit">login</button>
+      </div>
+    </form>
+  )
 }
 
 LoginForm.propTypes = {
-    handleLogin: PropTypes.func.isRequired,
-    handleUsernameChange: PropTypes.func.isRequired,
-    handlePasswordChange: PropTypes.func.isRequired,
-    username: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired
-  }
+  handleLogin: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
+}
 
 export default LoginForm
