e76397d03ff9e174e0b9b13dbe4a97637229f5d3
ojakoo <ilari.ojakorpi@gmail.com>
2020-11-16 16:36:48 +0200

    db.json

--- part06/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part06/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,27 +1,6 @@
 
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) => {
   switch (action.type) {
     case 'VOTE': 
       state = state.map(anecdote => anecdote.id !== action.id ? anecdote : {
@@ -31,7 +10,10 @@ const anecdoteReducer = (state = initialState, action) => {
       })
       return state
     case 'NEW_ANECDOTE':
+      console.log(action.data)
       return [ ...state, action.data ]
+    case 'INIT_ANECDOTES':
+      return action.data
     default: 
       return state
   }
@@ -44,11 +26,18 @@ export const vote = (id) => {
   }
 }
 
-export const createAnecdote = (content) => {
+export const createAnecdote = (anecdote) => {
   return {
     type: 'NEW_ANECDOTE',
-    data: asObject(content)
+    data: anecdote
   } 
+}
+
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data: anecdotes,
+  }
 }
 
 export default anecdoteReducer
