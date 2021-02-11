b43b610c7926bf7db7ce7b0d4828c1ae2082a1af
ojakoo <ilari.ojakorpi@gmail.com>
2021-02-11 18:35:45 +0200

    fix submodule

---
+++ part08/frontend/src/components/Authors.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+import { useQuery, useMutation } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../quaries'
+
+const Authors = (props) => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const [ editAuthor ] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [ { query: ALL_AUTHORS }]
+  })
+
+  const result = useQuery(ALL_AUTHORS)
+
+  if (!props.show) {
+    return null
+  }
+  
+  const submit = async (event) => {
+    event.preventDefault()
+
+    editAuthor({ variables: { name, setBornTo: Number(born) } })
+
+    setName('')
+    setBorn('')
+  }
+
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {result.data.allAuthors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <h2>set birthyear</h2>
+      <form onSubmit={submit}>
+        <label>
+          name
+          <select value={name} onChange={({ target }) => setName(target.value)}>
+            {result.data.allAuthors.map(author => 
+              <option key={author.name} value={author.name} >{author.name}</option>
+            )}
+          </select>
+        </label>
+        <div>
+          born
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type='submit'>update author</button>
+      </form>
+    </div>
+  )
+}
+
+export default Authors
